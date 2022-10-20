417cdd65a1e266969fab6f89381e8ee7ea4c51ba
lubwama <bizzicole87@gmail.com>
2022-10-20 16:18:30 +0300

    mvp completed

---
+++ ui/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex w-full h-10 gap-4 px-4 pt-4 pb-20 font-sans text-white md:px-20 md:gap-10">
+      <Link href="/">
+        <a className="text-2xl no-underline hover:text-slate-300">
+          Autodidact
+        </a>
+      </Link>
+      <Link href="/collection">
+        <a className="text-2xl no-underline text-grey-darkest hover:text-slate-300 grow">
+          Collection 😎
+        </a>
+      </Link>
+      <WalletMultiButton />
+    </nav>
+  )
+}
---
+++ ui/lib/constants.ts
@@ -0,0 +1,4 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const CANDY_MACHINE_ADDRESS = new PublicKey("2jR9vQihQrDGBYQKQocvcS1nK71S3UBPJLq9s7nzHySR")
+export const COLLECTION_MINT_ADDRESS = new PublicKey("52LL1hBWBVamieMgrM32k4D5t4LvvH2NcTojH9Qet1Ys")
