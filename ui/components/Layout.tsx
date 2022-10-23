4f48144612b6dccf79a28d768d7ddc9d9539adbe
Lubwama Collins <56563537+greatsage-raphael@users.noreply.github.com>
2022-10-23 18:22:37 +0300

    Update Layout.tsx

--- ui/components/Layout.tsx
+++ ui/components/Layout.tsx
@@ -3,11 +3,13 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
+   <>
     <div className="container flex flex-col items-center min-h-screen p-4 mx-auto max-w-7xl">
       <Navbar />
       {children}
 
     </div>
     <Footer />
+      </>
   )
 }
